f94fb110e0b800a361ee39209fd7bb2213d147ac
Andrea Ottolina <3297+sgabello@users.noreply.github.com>
2021-05-11 01:39:16 -0500

    Add adapters configurations

--- PIXAdManagerDemo/PIXAdManagerDemo/ViewController.m
+++ PIXAdManagerDemo/PIXAdManagerDemo/ViewController.m
@@ -24,8 +24,9 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     NSDictionary *testMopubConfiguration = @{@"adUnitID": @"0ac59b0996d947309c33f59d6676399f"};
-    NSDictionary *testAdmobConfiguration = @{@"appUnitID": @"ca-app-pub-3940256099942544~1458002511",
-                                             @"adUnitID": @"ca-app-pub-3940256099942544/2934735716"};
+    NSDictionary *testAdmobConfiguration = @{@"adUnitID": @"ca-app-pub-3940256099942544/2934735716"};
+    NSDictionary *mopubConfiguration = @{@"adUnitID": @"2e8c909d6e114a67aad04c5ca014f85c"};
+    NSDictionary *adMobConfiguration = @{@"adUnitID": @"ca-app-pub-3008186008208131/8209379850"};
     
     PIXAdManager *adManager = [PIXAdManager sharedManager];
     [adManager initMediationPartner:MediationPartnerMoPub withConfiguration:testMopubConfiguration];
