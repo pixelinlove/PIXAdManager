1ff6e919fd7f1fa3ceebd8c5c84e8d60890c2612
Andrea Ottolina <3297+sgabello@users.noreply.github.com>
2022-11-03 19:27:19 +0000

    Add empty swift file to generate bridging header to swift static pods

---
+++ PIXAdManagerDemo/PIXAdManagerDemo/Empty.swift
@@ -0,0 +1,15 @@
+//
+//  Empty.swift
+//  PIXAdManagerDemo
+//
+//  Created by Andrea Ottolina on 03/11/2022.
+//  Copyright © 2022 Andrea Ottolina. All rights reserved.
+//
+
+import Foundation
+
+// WARNING //
+// Facebook Audience Network require dynamic linkage by default
+// If want to use static linkage it is required to add an empty swift file to the project
+// Refer to: https://developers.facebook.com/docs/audience-network/setting-up/platform-setup/ios/add-sdk/#notes
+
